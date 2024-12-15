144f8324a8520e6deefcd9080e0f4705c352a380
gunshop-eikoh <toshiki-komatsu@eikoh-gun.com>
2024-12-15 16:15:13 +0900

    タイトル等の整備

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export', // 静的エクスポートを有効化
-  distDir: 'build', // 出力フォルダを指定
   assetPrefix: isProd ? './' : '', // 本番環境で相対パスを使用
   trailingSlash: true, // URL末尾にスラッシュを追加
   experimental: {
